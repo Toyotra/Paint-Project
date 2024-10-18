ba4a202d18baca3faf22489bb741e47a85b2e866
Jad Menkara <Jadmenkara@gmail.com>
2024-10-18 13:18:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame as pg
 import math
 import sys
+
+
 screen = pg.display.set_mode((1500,800))
 screen.fill((50,50,50))
-while True:
+
+running =  True
+while running:
     for event in pg.event.get():
-        if event.type=pg.quit():
+        if event.type==pg.quit():
+            running=False
             pg.quit()
             sys.exit()
 
